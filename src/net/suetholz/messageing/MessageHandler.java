bd48cfb0430ee1f03f61db70296fcf1185ba19a9
Bill S. <wj@suetholz.net>
2014-09-15 18:22:47 -0500

    Added Setters and Getters to the MessageHaneler Class.

--- src/net/suetholz/messageing/MessageHandler.java
+++ src/net/suetholz/messageing/MessageHandler.java
@@ -14,22 +14,39 @@ import net.suetholz.messageing.api.MessageProducer;
  * @author wsuetholz
  */
 public class MessageHandler implements Message {
+
     private MessageProducer producer;
     private MessageConsumer consumer;
-    
-    public MessageHandler ( MessageProducer producer, MessageConsumer consumer ) {
+
+    public MessageHandler(MessageProducer producer, MessageConsumer consumer) {
+	this.setProducer(producer);
+	this.setConsumer(consumer);
+    }
+
+    public MessageProducer getProducer() {
+	return producer;
+    }
+
+    public void setProducer(MessageProducer producer) {
 	if (producer == null) {
-	    throw new IllegalArgumentException ( "Message Producer not allowed to be null!");
-	}
-	if (consumer == null) {
-	    throw new IllegalArgumentException ( "Message Consumer not allowed to be null!");	    
+	    throw new IllegalArgumentException("Message Producer not allowed to be null!");
 	}
 	this.producer = producer;
+    }
+
+    public MessageConsumer getConsumer() {
+	return consumer;
+    }
+
+    public void setConsumer(MessageConsumer consumer) {
+	if (consumer == null) {
+	    throw new IllegalArgumentException("Message Consumer not allowed to be null!");
+	}
 	this.consumer = consumer;
     }
-    
+
     @Override
-    public void processMessage () {
-	this.consumer.consumeMessage ( this.producer ) ;
+    public void processMessage() {
+	this.consumer.consumeMessage(this.producer);
     }
 }
